512a6f30e173fdf3b9c1e9040ff9fabbb91a4b7a
Tirth88663 <tirthp88663@gmail.com>
2023-12-26 23:34:12 +0530

    day-4 arrays

---
+++ 4-array/src/PermutationArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PermutationArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i]=in.nextInt();
+        }
+        System.out.println(Arrays.toString(buildArray(a)));
+    }
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
